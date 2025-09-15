bfdb24b411c6444d9ebfb5269423521ef06ef07c
Kirill Popov <diplomatgmg@gmail.com>
2025-09-16 01:16:57 +0300

    chore: increased parsers schedule
    Took 6 minutes

--- services/vacancy-parser/src/celery_app/beat.py
+++ services/vacancy-parser/src/celery_app/beat.py
@@ -6,8 +6,8 @@ from celery.schedules import schedule
 __all__ = ["beat_schedule"]
 
 beat_schedule = {
-    "parse-vacancies-every-30-minutes": {
+    "parse-vacancies-every-10-minutes": {
         "task": "parse_vacancies",
-        "schedule": schedule(run_every=timedelta(minutes=30)),
+        "schedule": schedule(run_every=timedelta(minutes=10)),
     }
 }
--- services/vacancy-processor/src/celery_app/beat.py
+++ services/vacancy-processor/src/celery_app/beat.py
@@ -6,8 +6,8 @@ from celery.schedules import schedule
 __all__ = ["beat_schedule"]
 
 beat_schedule = {
-    "process-vacancies-every-1-minute": {
+    "process-vacancies-every-5-minute": {
         "task": "process_vacancies",
-        "schedule": schedule(run_every=timedelta(minutes=1)),
+        "schedule": schedule(run_every=timedelta(minutes=5)),
     }
 }
