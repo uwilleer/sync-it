32fd34afc53ad02187dc80b4495b733442b757f4
Kirill Popov <diplomatgmg@gmail.com>
2026-02-22 16:43:21 +0200

    feat: added g4f

--- services/gpt-api/src/services/gpt.py
+++ services/gpt-api/src/services/gpt.py
@@ -1,45 +1,38 @@
 import asyncio
+from typing import cast
 
 from aiohttp import ClientResponseError
 from common.logger import get_logger
 from common.shared.decorators.concurency import limit_requests
-from core.config import service_config
-from google import genai
+from g4f.Provider import You  # type: ignore[import-untyped]
+from g4f.client import AsyncClient  # type: ignore[import-untyped]
+from g4f.typing import Message  # type: ignore[import-untyped]
 
 
 logger = get_logger(__name__)
 
-MODELS = (
-    "models/gemini-3-flash-preview",
-    "models/gemini-3-pro-preview",
-    "models/gemini-pro-latest",
-    "models/gemini-2.5-pro",
-    "models/gemini-2.5-flash",
-    "models/gemma-3-27b-it",
-    "models/gemma-3-12b-it",
-    "models/gemma-3-4b-it",
-)
-
-MAX_RETRIES = len(MODELS)
+MAX_RETRIES = 3
 RETRY_DELAY = 3
 
-client = genai.Client(api_key=service_config.gemini_api_key.get_secret_value())
+client = AsyncClient()
 
 
-@limit_requests(16)
+@limit_requests(64)
 async def get_gpt_response(prompt: str) -> str | None:
-    for attempt, model in enumerate(MODELS, start=1):
+    message = Message(role="user", content=prompt)
+
+    for attempt in range(1, MAX_RETRIES + 1):
         try:
-            response = client.models.generate_content(
-                model=model,
-                contents=prompt,
+            response = await client.chat.completions.create(
+                provider=You,
+                messages=[message],
             )
+            return cast("str", response.choices[0].message.content)
+
         except ClientResponseError as e:
             logger.exception("Failed to get GPT response on attempt %s", attempt, exc_info=e)
-            if attempt > MAX_RETRIES:
+            if attempt == MAX_RETRIES:
                 return None
             await asyncio.sleep(RETRY_DELAY)
-        else:
-            return response.text
 
     return None
